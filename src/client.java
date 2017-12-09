bb7676550a150d159169c9381945cdb54d8d8c4d
Vidish <vidishnaik0509@gmail.com>
2017-12-09 22:40:20 +0530

    Initial Commit

---
+++ src/client.java
@@ -0,0 +1,77 @@
+import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URI;
+import java.util.Scanner;
+
+import static java.awt.Desktop.getDesktop;
+
+public class client {
+
+    static String message;
+
+    public static void main(String[] args) {
+        send();
+    }
+
+    public static void send() {
+        try {
+            Socket s = new Socket("localhost", 6666);
+            Scanner sc = new Scanner(System.in);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            System.out.println("Enter a website name: ");
+            message = sc.next();
+            dout.writeUTF(message);
+            dout.flush();
+            dout.close();
+            s.close();
+        } catch (IOException e) {
+        }
+        receive();
+    }
+
+    public static void receive() {
+        String str = "";
+        try {
+            ServerSocket ss = new ServerSocket(6670);
+            Socket s = ss.accept();
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            str = dis.readUTF();
+            ss.close();
+            System.out.println("IP : " + str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (!str.contains("not")) {
+            int n;
+            do {
+                System.out.println("Do you want to open the IP " + str + "?");
+                System.out.println("1. Yes");
+                System.out.println("2. No");
+                n = new Scanner(System.in).nextInt();
+                switch (n) {
+                    case 1:
+                        if (Desktop.isDesktopSupported()) {
+                            try {
+                                if (message.contains("www."))
+                                    getDesktop().browse(new URI(message));
+                                else
+                                    getDesktop().browse(new URI("www." + message));
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        System.out.println("Wrong Input");
+                }
+            } while (n != 1 && n != 2);
+        }
+        send();
+    }
+}
